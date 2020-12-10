87b98edb0e5ba647d8bec9464da7f814bd6259cd
tijgerkaars <tijgerkaars@users.noreply.github.com>
2020-12-10 14:28:23 +0100

    t

--- Python_Scripts/py39/Roll_Logger.py/Main.py
+++ Python_Scripts/py39/Roll_Logger.py/Main.py
@@ -1,23 +1,50 @@
-import tkiner as tk
+import tkinter as tk
+import json
+from tkinter import messagebox
+from datetime import datetime
+
 
 
 class Anim:
-    def __init__(self):
+    def __init__(self, *arg, **args):
+        args = {key.lower(): value for key,value in args.items()}
         self.root = tk.Tk()
         self.root.title( ' test ' )
-        self.root.bind('<Escape>', self.close_app)
+        self.root.protocol("WM_DELETE_WINDOW", self.on_closing)
+        self.root.bind('<Escape>', self.on_closing)
+        try:
+            self.debug = args['debug']
+        except:
+            self.debug = False
+            pass
         self.entry_size = 10
+
+    def on_closing(self,event=None):
+        if not self.debug:
+            if messagebox.askokcancel("Quit", "Do you want to quit?"):
+                self.root.destroy()
 
     def close_app(self, event=None):
         self.root.destroy()
 
 class Dice_counter(Anim):
-    def __init__(self):
-        super().__init__()
+    def __init__(self,*arg, **args):
+        super().__init__(*arg, **args)
+
+
+        self.json_file = r'Roll_Logger.py\logg.json'
+        try:
+            with open(self.json_file, 'r') as f:
+                print(f)
+                setting = json.load(f)
+                self.data = setting
+        except:
+            self.clear_data()
+
+        self.clear_data()
+
         self.root.title( 'Dice Logger' )
-        self.names =[
-            'New Name'
-        ]
+        self.names = [ 'New Name' ]
         self.dice = [ f'd{i}' for i in [4,6,8,10,12,20]] + ['New' ]
 
         self.widget = {
@@ -28,10 +55,40 @@ class Dice_counter(Anim):
         }
 
         self.root.mainloop()
-    `
-    def create_Lable(self):
+
+    def clear_data(self):
+        self.data = {}
+        self.data['START'] = {
+                    'NAMES' : [],
+                    'DICE'  : [],
+                    'TYPES' : []     }
+        self.data['LOG'] = {}
+
+        self.add_log( '','','','','' )
+
+    def add_log( self, name, dice, types, amount, comment ):
+        now = datetime.now()
+        day = now.date(); time = now.time()
+        try:
+            self.data['LOG'][day][]
+        print(day)      #'%Y-%m-%d'))
+        print(time)     #'%H:%M:%S'))
+
+        '%d/%m/%y %H:%M:%S'
+
+    def on_closing(self,event=None):
+        if not self.debug:
+            if messagebox.askokcancel("Quit", "Do you want to quit?"):
+                with open(self.json_file, 'w') as f:
+                    f.write(json.dumps(self.data, indent=4))
+        self.root.destroy()
+
+    def create_Frame(self):
         pass
     
 
     def create_dropdown(self, x,y):
         pass
+
+if __name__ == "__main__":
+    ui = Dice_counter()
